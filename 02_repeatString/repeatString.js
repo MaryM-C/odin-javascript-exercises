cb1e7105316ec627730a889131a186110c944586
MaryCalamba <calambamarymaee@gmail.com>
2024-02-29 21:51:43 +0800

    Modify files to meet the exercises' requirements repeatString.js - added functions to modify the given string be repeated based on the number given reverseString.js - added functions that reverses a string. (Note: The answer is less optimized than the Odin's) repeatString.spec.js and revserString.spec.js - modified the files to enable the other tests written

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let resultString = "";
 
+    if(validateNumber(num)) {
+        for (let index = 0; index < num; index++) {
+            resultString += string;
+        }
+    } else {
+        resultString = 'ERROR'; 
+    }
+
+    return resultString;
 };
+
+function validateNumber(number) {
+    if(number < 0) {
+        return false;
+    }
+    return true;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversedString = "";
+    for (let index = string.length-1; index >= 0; index--) {
+        reversedString += string.charAt(index);
+    }
 
+    return reversedString;
 };
 
 // Do not edit below this line
